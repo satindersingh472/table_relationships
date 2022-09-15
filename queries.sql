7c3b2d93184773ac7a8401221dfb0900195ac598
satindersingh772 <92008199+satindersingh772@users.noreply.github.com>
2022-09-14 20:55:01 -0600

    finished

--- queries.sql
+++ queries.sql
@@ -5,8 +5,8 @@ update blogger set password = 'bad_password' where password = 'pass'
 SELECT bp.content,bp.title,b.username
 from blogger b inner join blog_post bp on b.id = bp.blogger_id; 
 
-SELECT bpc.content, bp.title,b.username
-from blogger b inner join blog_post bp on b.id = bp.blogger_id inner join blog_post_comment bpc  on  bp.id = bpc.blog_post_id; 
+SELECT bpc.content,bp.title,b.username 
+from blogger b inner join blog_post_comment bpc  on  b.id = bpc.blogger_id inner join blog_post bp on bp.id = bpc.blog_post_id; 
 
 SELECT id,content from blog_post_comment bpc where content like '%delete me%';
 DELETE from blog_post_comment where content like '%delete me%';
--- table_relationships.sql
+++ table_relationships.sql
@@ -1,8 +1,8 @@
--- MariaDB dump 10.19  Distrib 10.6.7-MariaDB, for debian-linux-gnu (x86_64)
+-- MariaDB dump 10.19  Distrib 10.8.3-MariaDB, for osx10.17 (x86_64)
 --
 -- Host: localhost    Database: table_relationships
 -- ------------------------------------------------------
--- Server version	10.6.7-MariaDB-2ubuntu1.1
+-- Server version	10.8.3-MariaDB
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
@@ -40,7 +40,11 @@ CREATE TABLE `blog_post` (
 
 LOCK TABLES `blog_post` WRITE;
 /*!40000 ALTER TABLE `blog_post` DISABLE KEYS */;
-INSERT INTO `blog_post` VALUES ('honesty is always the best option to choose','honesty is the best policy','2021-05-06',1,1),('tiger is an extinct animal','extinct species','2022-01-03',2,3),('climate changing is affecting us ','climate change','2021-05-08',1,4),('bigger cities have more traffic','increasing traffic','2022-01-06',2,5);
+INSERT INTO `blog_post` VALUES
+('honesty is always the best option to choose','honesty is the best policy','2021-05-06',1,1),
+('tiger is an extinct animal','extinct species','2022-01-03',2,3),
+('climate changing is affecting us ','climate change','2021-05-08',1,4),
+('bigger cities have more traffic','increasing traffic','2022-01-06',2,5);
 /*!40000 ALTER TABLE `blog_post` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -71,7 +75,20 @@ CREATE TABLE `blog_post_comment` (
 
 LOCK TABLES `blog_post_comment` WRITE;
 /*!40000 ALTER TABLE `blog_post_comment` DISABLE KEYS */;
-INSERT INTO `blog_post_comment` VALUES ('you are sure about honesty','2021-05-07',1,1,1),('yes!! tigers are!!','2022-01-03',2,3,2),('yes honestly always is','2021-05-06',1,1,3),('true!!!','2021-05-07',1,1,4),('well!! we are creating pollution','2021-05-08',1,4,6),('yes climate change is real','2021-05-09',1,4,7),('carbon emissions are on the rise','2021-05-10',1,4,8),('tigers are deceasing day by day','2022-01-05',2,3,9),('we should stop hunting tigers','2022-01-07',2,3,10),('traffic is a real problem','2022-01-06',2,5,11),('traffic kills many people','2022-01-09',2,5,12),('traffic makes us late everytime','2022-01-11',2,5,13),('root cause delete me of accidents','2022-01-04',2,5,16);
+INSERT INTO `blog_post_comment` VALUES
+('you are sure about honesty','2021-05-07',4,1,1),
+('yes!! tigers are!!','2022-01-03',1,3,2),
+('yes honestly always is','2021-05-06',2,1,3),
+('true!!!','2021-05-07',3,1,4),
+('well!! we are creating pollution','2021-05-08',1,4,6),
+('yes climate change is real','2021-05-09',2,4,7),
+('carbon emissions are on the rise','2021-05-10',3,4,8),
+('tigers are deceasing day by day','2022-01-05',4,3,9),
+('we should stop hunting tigers','2022-01-07',2,3,10),
+('traffic is a real problem','2022-01-06',3,5,11),
+('traffic kills many people','2022-01-09',4,5,12),
+('traffic makes us late everytime','2022-01-11',2,5,13),
+('root cause delete me of accidents','2022-01-04',3,5,16);
 /*!40000 ALTER TABLE `blog_post_comment` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -92,7 +109,7 @@ CREATE TABLE `blogger` (
   PRIMARY KEY (`id`),
   UNIQUE KEY `blogger_unique_username` (`username`),
   UNIQUE KEY `blogger_unique_email` (`email`)
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -101,7 +118,11 @@ CREATE TABLE `blogger` (
 
 LOCK TABLES `blogger` WRITE;
 /*!40000 ALTER TABLE `blogger` DISABLE KEYS */;
-INSERT INTO `blogger` VALUES ('username_one','123456','user_one','2018-01-03','user_one@email.com',1),('username_two','123456','user_two','2020-01-01','user_two@email.com',2),('username_three','pass','user_three','2021-04-06','user_three@email.com',3);
+INSERT INTO `blogger` VALUES
+('username_one','123456','user_one','2018-01-03','user_one@email.com',1),
+('username_two','123456','user_two','2020-01-01','user_two@email.com',2),
+('username_three','pass','user_three','2021-04-06','user_three@email.com',3),
+('username_four','123456','user_four','2021-04-07','user_four@email.com',4);
 /*!40000 ALTER TABLE `blogger` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -118,4 +139,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2022-09-14 12:43:34
+-- Dump completed on 2022-09-14 20:54:37
